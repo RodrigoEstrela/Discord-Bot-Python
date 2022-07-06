5a79f5268fa7856fd31e3f22cef4bcb529813a64
rdas-nev <restrela20@gmail.com>
2022-07-06 19:23:31 +0100

    progress yeah

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 # bot.py
 import os
+import random
 
-import discord
+#import discord
+
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,12 +12,13 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 
-intents = discord.Intents.default()
-intents.members = True
-client = discord.Client(intents=intents)
+#intents = discord.Intents.default()
+#intents.members = True
+#client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!')
 
-
-@client.event
+'''
+@client.event  # on_ready
 async def on_ready():
     guild = discord.utils.get(client.guilds, name=GUILD)
     print(
@@ -24,4 +28,53 @@ async def on_ready():
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
-client.run(TOKEN)
+
+@client.event  # on_member_join
+async def on_member_join(member):
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hello there, {member.name}, welcome to {guild.name}!'
+    )
+
+
+@client.event  # on_message
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == '!random_nu':
+        await message.channel.send(random.randint(0, 4243))
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+'''
+
+
+@bot.command(name='42', help='Shows an objective truth about 42')
+async def quarentaedois(ctx):
+    objective_truths = [
+        '42 is amazing!',
+        '42 is the best school!',
+        '42 mega elite!',
+        '42 network > every other school',
+        '42 gigachad school'
+    ]
+
+    selected_truth = random.choice(objective_truths)
+    await ctx.send(selected_truth)
+
+
+@bot.command(name='sentido_da_vida', help='Enlightens the user!')
+async def life_meaning(ctx):
+    await ctx.send('42, a tua mãe de 4 e eu de 2.\n   -Antonio Vieira, 2022')
+
+
+'''@client.event  # on_error
+async def on_error(event, *args):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'\nUnhandled message: {args[0]}1n\n\n')
+        else:
+            raise
+'''
+bot.run(TOKEN)
+
